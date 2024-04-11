18bb599b242724ea38e46613321a2daf22de02cb
Nitin Mishra <50167912+Nitin2806@users.noreply.github.com>
2024-04-11 17:39:11 -0400

    Added: Added class files and images

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class Candidate {
+  final String name;
+  final String imageUrl;
+  final String position;
+  final int userID;
+
+  Candidate({
+    required this.userID,
+    required this.name,
+    required this.imageUrl,
+    required this.position,
+  });
+}
