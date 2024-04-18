c9cf2c32315b49a6dfd776aec46cef93a7239f0f
bhojani-deep <deep.180670107008@gmail.com>
2024-04-17 20:08:37 -0400

    Fixed functionalities for candidate details layout

--- lib/screens/candidate_detail.dart
+++ lib/screens/candidate_detail.dart
@@ -4,7 +4,9 @@ import '../models/user.dart';
 class CandidateDetail extends StatelessWidget {
   final Candidate candidate;
 
-  const CandidateDetail({super.key, required this.candidate});
+  const CandidateDetail(
+      {super.key,
+      required this.candidate}); // Get the candidate information from candidate page
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +14,15 @@ class CandidateDetail extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Profile"),
       ),
+      //Scrollview for handling the page
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          // Add all information in column
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
+              //Display image of the user
               child: CircleAvatar(
                 backgroundImage: NetworkImage(candidate.imageUrl),
                 radius: 100,
@@ -33,9 +38,11 @@ class CandidateDetail extends StatelessWidget {
               candidate.bio,
               style: TextStyle(fontSize: 18, color: Colors.grey[700]),
             ),
+            //Other information about the user
             const SizedBox(height: 20),
             const Divider(),
             const SizedBox(height: 20),
+            // Using a function to store information
             _buildDetailRow('Company', candidate.company),
             const SizedBox(height: 12),
             _buildDetailRow('Location', candidate.location),
@@ -49,6 +56,7 @@ class CandidateDetail extends StatelessWidget {
     );
   }
 
+//Get the data for label and value to display information's to decrease the code for same elemments
   Widget _buildDetailRow(String label, String value) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
